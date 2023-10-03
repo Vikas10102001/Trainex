dbc9a61af9b236ab98b5483c3cf134d4a2b88a29
Vikas <provikas2001@gmail.com>
2023-10-04 00:31:01 +0530

    updated dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,37 +1,36 @@
 import React from "react";
-import AppLayout from "../shared/AppLayout";
-import appointments from "../../data/apointment.json";
-import clients from "../../data/client.json";
+import AppLayout from "../shared/ui/AppLayout";
 import "../../styles/dashboard.css";
 import Status from "./Status";
 import ClientAppointment from "./ClientAppointment";
-
-const clientInfoWithAppointments = {};
-
-for (const appointment of appointments) {
-  const clientId = appointment.client;
-
-  if (!clientInfoWithAppointments[clientId]) {
-    clientInfoWithAppointments[clientId] = {
-      clientId: clientId,
-      firstName: clients[clientId - 1].firstName,
-      lastName: clients[clientId - 1].lastName,
-      location: clients[clientId - 1].address,
-      appointments: [],
-    };
-  }
-
-  clientInfoWithAppointments[clientId].appointments.push({
-    date: appointment.date,
-    time: appointment.time,
-  });
-}
-
-const clientAppointment = Object.values(clientInfoWithAppointments);
-
-console.log(clientAppointment);
+import { useSelector } from "react-redux";
 
 export default function Dashboard() {
+  const clients = useSelector((state) => state.client.data);
+  const appointments = useSelector((state) => state.appointment.data);
+  const clientInfoWithAppointments = {};
+
+  for (const appointment of appointments) {
+    const clientId = appointment.client;
+
+    if (!clientInfoWithAppointments[clientId]) {
+      clientInfoWithAppointments[clientId] = {
+        clientId: clientId,
+        firstName: clients[clientId - 1].firstName,
+        lastName: clients[clientId - 1].lastName,
+        location: clients[clientId - 1].address,
+        appointments: [],
+      };
+    }
+
+    clientInfoWithAppointments[clientId].appointments.push({
+      id: appointment.id,
+      date: appointment.date,
+      time: appointment.time,
+    });
+  }
+
+  const clientAppointment = Object.values(clientInfoWithAppointments);
   return (
     <AppLayout>
       <Status clientAppointment={clientAppointment} />
