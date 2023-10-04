14df252c2d8afdf761b2491d0ebcccbe77f26969
Vikas <provikas2001@gmail.com>
2023-10-04 19:18:46 +0530

    updated clientAppointment data

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -8,29 +8,39 @@ import { useSelector } from "react-redux";
 export default function Dashboard() {
   const clients = useSelector((state) => state.client.data);
   const appointments = useSelector((state) => state.appointment.data);
+
+  // Create an object to store client data with appointments
   const clientInfoWithAppointments = {};
 
-  for (const appointment of appointments) {
+  // Populate clientInfoWithAppointments with client data
+  clients.forEach((client) => {
+    const clientId = client.id;
+    clientInfoWithAppointments[clientId] = {
+      clientId,
+      firstName: client.firstName,
+      lastName: client.lastName,
+      location: client.address,
+      appointments: [],
+    };
+  });
+
+  // Populate clientInfoWithAppointments with appointments
+  appointments.forEach((appointment) => {
     const clientId = appointment.client;
+    const clientInfo = clientInfoWithAppointments[clientId];
 
-    if (!clientInfoWithAppointments[clientId]) {
-      clientInfoWithAppointments[clientId] = {
-        clientId: clientId,
-        firstName: clients[clientId - 1].firstName,
-        lastName: clients[clientId - 1].lastName,
-        location: clients[clientId - 1].address,
-        appointments: [],
-      };
+    if (clientInfo) {
+      clientInfo.appointments.push({
+        id: appointment.id,
+        date: appointment.date,
+        time: appointment.time,
+      });
     }
+  });
 
-    clientInfoWithAppointments[clientId].appointments.push({
-      id: appointment.id,
-      date: appointment.date,
-      time: appointment.time,
-    });
-  }
-
+  // Convert the object into an array of client data
   const clientAppointment = Object.values(clientInfoWithAppointments);
+  console.log(clientAppointment);
   return (
     <AppLayout>
       <Status clientAppointment={clientAppointment} />
