c39d184819a722bb01d1171aaa0196cd72352440
Vikas <provikas2001@gmail.com>
2023-10-05 19:28:41 +0530

    Added calender page and components

--- src/components/Calender/Calender.jsx
+++ src/components/Calender/Calender.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/calendar.css";
 import AppLayout from "../shared/ui/AppLayout";
+import MonthNavigation from "./MonthNavigation";
+import CalenderMain from "./CalenderMain";
 
-export default function Calender() {
+function Calendar({ clientAppointment }) {
+  const currentDate = new Date();
+  const [currentMonth, setCurrentMonth] = useState(currentDate);
   return (
     <AppLayout>
-      <div>Calender</div>
+      <div className="calendar">
+        <MonthNavigation
+          currentMonth={currentMonth}
+          setCurrentMonth={setCurrentMonth}
+        />
+        <CalenderMain
+          currentMonth={currentMonth}
+          setCurrentMonth={setCurrentMonth}
+          clientAppointment={clientAppointment}
+        />
+      </div>
     </AppLayout>
   );
 }
+
+export default Calendar;
