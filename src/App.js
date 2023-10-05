608c9c48a01756c47c71eb6a48a5dc34fc6330ca
Vikas <provikas2001@gmail.com>
2023-10-05 19:33:09 +0530

    updated:Passing clientAppointment data from app

--- src/App.js
+++ src/App.js
@@ -6,14 +6,51 @@ import { useSelector } from "react-redux";
 import Alert from "./components/shared/component/Alert";
 
 function App() {
+  const clients = useSelector((state) => state.client.data);
+  const appointments = useSelector((state) => state.appointment.data);
+
+  const clientInfoWithAppointments = {};
+
+  clients.forEach((client) => {
+    const clientId = client.id;
+    clientInfoWithAppointments[clientId] = {
+      clientId,
+      firstName: client.firstName,
+      lastName: client.lastName,
+      location: client.address,
+      gender: client.gender,
+      appointments: [],
+    };
+  });
+
+  appointments.forEach((appointment) => {
+    const clientId = appointment.client;
+    const clientInfo = clientInfoWithAppointments[clientId];
+
+    if (clientInfo) {
+      clientInfo.appointments.push({
+        id: appointment.id,
+        date: appointment.date,
+        time: appointment.time,
+      });
+    }
+  });
+
+  const clientAppointment = Object.values(clientInfoWithAppointments);
   const alertData = useSelector((state) => {
     return state.alert.data;
   });
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/calender" element={<Calender />} />
+        <Route
+          path="/"
+          element={<Dashboard clientAppointment={clientAppointment} />}
+        />
+        <Route
+          path="/calender"
+          element={<Calender clientAppointment={clientAppointment} />}
+        />
       </Routes>
       {alertData && <Alert data={alertData} />}
     </>
