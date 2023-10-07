3a64f77d6d418cbea611242b21203307e5d7aa7b
Vikas <provikas2001@gmail.com>
2023-10-07 08:08:59 +0530

    set active styles for link

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React  from "react";
 import "../../styles/navigation.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 export default function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const handleCalendarButton = () => {
+    navigate("/calendar");
+  };
+  const handleDashboardButton = () => {
+    navigate("/");
+  };
   return (
     <div className="navbar">
       <div className="left">
         <div className="logo">
-          <img src="/logo-white.png" alt="logo" />
+          <Link to="/">
+            <img src="/logo-white.png" alt="logo" />
+          </Link>
         </div>
-        <span>Trainex</span>
+        <span className="logo-name">Trainex</span>
       </div>
       <div className="right">
-        <Link to="/">Dashboard</Link>
-        <Link to="/calender">Calender</Link>
+        <button
+          className={`${currentPath === "/" ? "active" : ""}`}
+          onClick={handleDashboardButton}
+        >
+          Dashboard
+        </button>
+        <button
+          onClick={handleCalendarButton}
+          className={`${currentPath === "/calendar" ? "active" : ""}`}
+        >
+          Calendar
+        </button>
       </div>
     </div>
   );
