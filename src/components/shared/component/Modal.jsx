92cf7281196bf5e45577c2dca7eed852121d3054
Vikas <provikas2001@gmail.com>
2023-10-07 08:15:28 +0530

    remove body scroll when modal is active

--- src/components/shared/component/Modal.jsx
+++ src/components/shared/component/Modal.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import { Close } from "@mui/icons-material";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const Modal = ({ isOpen, onConfirm, onCancel, title, footer, data }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "auto";
+    }
+    return () => {
+      document.body.style.overflowY = "auto";
+    };
+  }, [isOpen]);
   return ReactDOM.createPortal(
-    <div className={`modal ${isOpen ? "is-active" : ""}`}>
+    <div className={`modal ${!isOpen ? "is-inactive" : "is-active"}`}>
       <div className="modal-background" onClick={onCancel}></div>
       <div className="modal-card">
         <header className="modal-card-head">
           <p className="modal-card-title">{title}</p>
-          <button
-            className="delete"
-            aria-label="close"
-            onClick={onCancel}
-          ></button>
         </header>
         <section className="modal-card-body">{data}</section>
         <footer className="modal-card-foot">{footer}</footer>
